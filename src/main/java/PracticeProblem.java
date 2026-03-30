3906f9aa4f549b84b6bbb9b5ecc2acb3756bc694
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-03-30 13:58:16 +0000

    solve the thing

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,34 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	// recursion sucks to explain 
+
+	public static int sumOfDigits(int x){
+		x = Math.abs(x);
+
+		//base case
+		if(x < 10){
+			return x;
+		}
+		
+		//return the sum of the last digit, and the sumOfDigits of the 10th place onward
+		//this means you sum up the digits backwards, cool.
+		return(sumOfDigits(x/10) + x%10);
+	}
+
+	public static int factorial(int num){
+
+		//base case
+		if(num == 0){
+			return 1;
+		} else if (num < 0){
+			return 0;
+		}
+
+		//return (n-1)! * n
+		return factorial(num-1)*num;
+	}
+
 
 	
 
